6fce1046fbadd4ae4e252d359e728068a98a6be0
Martin-Porsch <sekerabka@gmail.com>
2026-02-17 17:42:08 +0100

    dinal commfdfhuidsfjigfdgehgdfhfgdghfdhgfhfgdfg

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,71 @@
-console.log("nesnasim brokolici");
-console.log("honza");
+
+
+
+
+
+//inputs
+let rychlost:number = 14;
+let cas:number = 0;
+let draha:number = 78;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(rychlost === 0){
+
+    rychlost = draha / cas
+
+} else if(cas === 0){
+
+    cas = draha / rychlost
+
+} else if(draha === 0){
+
+    draha = rychlost * cas
+
+}
+
+cas = Math.round(cas);
+draha = Math.round(draha);
+rychlost = Math.round(rychlost);
+
+
+console.log(`Rychlost = ${rychlost} `)
+console.log(`Čas = ${cas}`)
+console.log(`Dráha = ${draha}`)
